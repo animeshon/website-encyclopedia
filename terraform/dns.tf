8cc9c9af2cb9d3a97e46745c33b7ab1ab3efb103
Christian Roggia <christian.roggia@gmail.com>
2020-11-12 10:01:46 +0100

    Disable DNS and IP creation

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -4,20 +4,20 @@ locals {
   zone_name       = data.terraform_remote_state.root.outputs.managed_zone_animeshon_com_name
 }
 
-# Setup global ingress ip address for the "insider" subdomain.
-resource "google_compute_global_address" "insider" {
-  name = "insider-animeshon-com"
-}
+# # Setup global ingress ip address for the "insider" subdomain.
+# resource "google_compute_global_address" "insider" {
+#   name = "insider-animeshon-com"
+# }
 
-# Setup "source" subdomain DNS record.
-resource "google_dns_record_set" "insider" {
-  project      = local.zone_project_id
-  name         = "insider.${local.zone_dns_name}"
-  managed_zone = local.zone_name
-  type         = "A"
-  ttl          = 300
+# # Setup "source" subdomain DNS record.
+# resource "google_dns_record_set" "insider" {
+#   project      = local.zone_project_id
+#   name         = "insider.${local.zone_dns_name}"
+#   managed_zone = local.zone_name
+#   type         = "A"
+#   ttl          = 300
 
-  rrdatas = [
-    google_compute_global_address.insider.address,
-  ]
-}
+#   rrdatas = [
+#     google_compute_global_address.insider.address,
+#   ]
+# }
