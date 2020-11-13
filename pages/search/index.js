c2c3856ee2238929f792ad3629e3f433e33620c8
Luscha <matteo.roggia@gmail.com>
2020-11-13 19:24:28 +0100

    infinite scroll

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,7 +1,9 @@
 import Link from 'next/link';
+import { useState, useEffect, useContext } from 'react';
 
 import kebabCase from 'lodash/kebabCase';
-import Router from 'next/router';
+import { withRouter } from 'next/router';
+import { SearchContext } from '@/ctx/search';
 
 import { performSearch, details } from '@/queries/search/Search';
 
@@ -16,121 +18,110 @@ import { withPremiereAny } from 'utilities/Premiere';
 import { withType } from 'utilities/MediaType';
 import { withSubType } from 'utilities/MediaSubtype';
 
-
-class Search extends React.Component {
-    constructor(props) {
-        super(props)
-    };
-
-    static getInitialProps = async ctx => {
-        const startTime = Date.now();
-
-        const searchTerm = ctx.query.q;
-        // max 10 + 90 results
-        const page = Math.min(ctx.query.p !== undefined ? ctx.query.p : 0, 9);
-
-        // No query, no results
-        if (searchTerm === undefined) {
-            return {}
-        }
-    
-        const client = ctx.apolloClient;
-        
-        const { results, hasMore } = await SearchQuery(client, searchTerm, page, []);
-        const queryTime = (Date.now() - startTime) / 1000.0; // in ms 
-        
-        return {queryTime, results, hasMore, searchTerm, page};
-    };
-
-    render() {
-        return (
-            <>
-                <Header isSearchAvailable />
-                <em className="results-displayer">
-                    Results displayed in {(this.props.queryTime).toFixed(2)} seconds
-                </em>
-                <div className="results-container">
-                    <div className="left-column">
-                    <InfiniteScroll
-                        dataLength={this.props.results.length}
-                        next={MoreResults(this.props.searchTerm, this.props.page + 1)}
-                        hasMore={this.props.hasMore}
-                        >
-                        {this.props.results.map((item, index) => {
-                            const primary = index <= 3 ? 'primary' : 'secondary';
-                            return (
-                                <article
-                                    key={item.id}
-                                    className={`search-result anime ${primary}-result`}
+const Search = ({ router, queryTime, results, hasMore, searchTerm, page }) => {
+    return (
+        <>
+            <Header isSearchAvailable />
+            <em className="results-displayer">
+                Results displayed in {(queryTime).toFixed(2)} seconds
+            </em>
+            <div className="results-container">
+                <div className="left-column">
+                <InfiniteScroll
+                    dataLength={results.length}
+                    next={() => router.push({
+                        pathname: '/search',
+                        query: { q: searchTerm, p: page+1},
+                    })}
+                    hasMore={hasMore}
+                    >
+                    {results.map((item, index) => {
+                        const primary = index <= 3 ? 'primary' : 'secondary';
+                        return (
+                            <article
+                                key={item.id}
+                                className={`search-result anime ${primary}-result`}
+                            >
+                                <Link
+                                    href="/[id]"
+                                    as={`/${item.id}_${kebabCase(item.title)}`}
                                 >
-                                    <Link
-                                        href="/[id]"
-                                        as={`/${item.id}_${kebabCase(item.title)}`}
-                                    >
-                                        <div className="search-result__row">
-                                            {item.profileImage && (
-                                                <figure className="search-result__image">
-                                                    <img
-                                                        src={item.profileImage}
-                                                        alt={`${item.title} Cover (${item.media})`}
-                                                    />
-                                                </figure>
-                                            )}
-                                            <header className="search-result__texts">
-                                                <div className="search-result__breadcrumb">
-                                                    <span>{item.media}</span>
-                                                    <span>{item.title}</span>
-                                                </div>
-                                                <h2>{item.title}</h2>
-                                                {item.type && (<strong>{item.type}</strong>)}
-                                                <p>{item.description}</p>
-                                            </header>
-                                        </div>
-                                    </Link>
-                                    {/* TODO children
-                                    {item.childContents && primary == 'primary' && (
-                                        <aside className="search-result__aside">
-                                            <h3>Episodes</h3>
-                                            <div className="search-result__column">
-                                                <AnimeEpisodes
-                                                    animeTitle={mainTitle}
-                                                    animeId={mainId}
-                                                    episodes={episodes}
+                                    <div className="search-result__row">
+                                        {item.profileImage && (
+                                            <figure className="search-result__image">
+                                                <img
+                                                    src={item.profileImage}
+                                                    alt={`${item.title} Cover (${item.media})`}
                                                 />
+                                            </figure>
+                                        )}
+                                        <header className="search-result__texts">
+                                            <div className="search-result__breadcrumb">
+                                                <span>{item.media}</span>
+                                                <span>{item.title}</span>
                                             </div>
-                                            <div className="search-result__more-trigger">
-                                                <Link
-                                                    href="/anime/[anime_id]/episodes"
-                                                    as={`/anime/${mainId}_${kebabCase(
-                                                        mainTitle,
-                                                    )}/episodes`}
-                                                >
-                                                    <button>more</button>
-                                                </Link>
-                                            </div>
-                                        </aside>
-                                    )} */}
-                                </article>
-                            );
-                        })}
-                    </InfiniteScroll>
-                    </div>
-                    <div className="right-column">
-                        {/* TODO Universes */}
-                    </div>
+                                            <h2>{item.title}</h2>
+                                            {item.type && (<strong>{item.type}</strong>)}
+                                            <p>{item.description}</p>
+                                        </header>
+                                    </div>
+                                </Link>
+                                {/* TODO children
+                                {item.childContents && primary == 'primary' && (
+                                    <aside className="search-result__aside">
+                                        <h3>Episodes</h3>
+                                        <div className="search-result__column">
+                                            <AnimeEpisodes
+                                                animeTitle={mainTitle}
+                                                animeId={mainId}
+                                                episodes={episodes}
+                                            />
+                                        </div>
+                                        <div className="search-result__more-trigger">
+                                            <Link
+                                                href="/anime/[anime_id]/episodes"
+                                                as={`/anime/${mainId}_${kebabCase(
+                                                    mainTitle,
+                                                )}/episodes`}
+                                            >
+                                                <button>more</button>
+                                            </Link>
+                                        </div>
+                                    </aside>
+                                )} */}
+                            </article>
+                        );
+                    })}
+                </InfiniteScroll>
                 </div>
-                <Footer contextualClass="search-footer" />
-            </>
-        );
-    }
+                <div className="right-column">
+                    {/* TODO Universes */}
+                </div>
+            </div>
+            <Footer contextualClass="search-footer" />
+        </>
+    );
 }
 
-const MoreResults = (search, page) => {
-    Router.push({
-        pathname: '/search',
-        query: { q: search, p: page },
-    });
-}
+Search.getInitialProps = async ctx => {
+    const startTime = Date.now();
+
+    const searchTerm = ctx.query.q;
+    // max 10 + 90 results
+    const page = Math.min(ctx.query.p !== undefined ? ctx.query.p : 0, 9);
+
+    // No query, no results
+    if (searchTerm === undefined) {
+        return {queryTime:0, results:[]}
+    }
+
+    const client = ctx.apolloClient;
+    
+    const { results, hasMore } = await SearchQuery(client, searchTerm, page, []);
+    const queryTime = (Date.now() - startTime) / 1000.0; // in ms 
+    
+    return {queryTime, results, hasMore, searchTerm, page};
+};
 
 const SearchQuery = async (client, searchTerm, pages, filter) => {
     const amountRequested = 10 + pages * 10
@@ -218,4 +209,4 @@ const ChildContents = ({ episodes, animeId, animeTitle }) => {
     });
 };
 
-export default Search;
+export default withRouter(Search);
