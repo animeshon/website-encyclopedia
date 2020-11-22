a40ac7052f4efe838043ce7f92c79f37ea5dff63
Christian Roggia <christian.roggia@gmail.com>
2020-11-22 15:45:16 +0100

    Move NEG into hompage repository

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,6 @@
 output "backend_service_id" {
-  value = google_compute_backend_service.encyclopedia.id
+  value = google_cloud_run_service.encyclopedia.id
 }
 output "backend_service_name" {
-  value = google_compute_backend_service.encyclopedia.name
+  value = google_cloud_run_service.encyclopedia.name
 }
