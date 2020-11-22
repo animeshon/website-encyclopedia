33102d8acc07f270fad135e8fd369e809e57ec5c
Christian Roggia <christian.roggia@gmail.com>
2020-11-22 04:18:46 +0100

    Add missing TF variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,10 @@
 provider "google" {
   version = "~> 3.31"
 }
+
+locals {
+  project_id     = data.terraform_remote_state.general.outputs.project_id
+  project_number = data.terraform_remote_state.general.outputs.project_number
+
+  sa_compute_email = data.terraform_remote_state.general.outputs.google_compute_default_service_account_email
+}
