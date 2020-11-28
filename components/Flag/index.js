1c9455f58a53fb2f818ac32be3ec3d8603e3568b
Luscha <matteo.roggia@gmail.com>
2020-11-28 12:35:44 +0100

    fix undefined nationality

--- components/Flag/index.js
+++ components/Flag/index.js
@@ -1,8 +1,7 @@
-import { noop } from 'lodash';
 import React from 'react';
 
 const Flag = ({ nationality }) => {
-    const n = nationality.toLowerCase();
+    const n = nationality?.toLowerCase();
     return (<>
         {n &&
             <span className={`fp fp-sm custom-fp ${n == 'en' ? 'gb' : n}`} />
