fd91ba161520d25918f534e403aa814f1fea5946
Luscha <matteo.roggia@gmail.com>
2020-12-02 00:48:01 +0100

    seo in the search result page

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,5 +1,8 @@
-import React from 'react';
-import { withRouter } from 'next/router';
+import React, { useContext } from 'react';
+import Head from 'next/head';
+import { withRouter, useRouter } from 'next/router';
+
+import { SearchContext } from '@/ctx/Search';
 
 import { performSearch, details } from '@/queries/search/Search';
 
@@ -10,20 +13,62 @@ import ResultDisplayer from '@/components/SearchResult/ResultDisplayer';
 
 import * as locale from '@/utilities/Localization';
 import * as image from '@/utilities/Image';
+import * as uri from '@/utilities/URI';
 import { PremiereAny } from '@/utilities/Premiere';
 import { Type } from '@/utilities/MediaType';
 import { Subtype } from '@/utilities/MediaSubtype';
 import { ExecuteQuery, ExecuteQueries, PrepareQuery } from '@/utilities/Query';
 
-const Search = ({ queryTime, results, hasMore, searchTerm, page }) => {
+const WEBSITE_NAME = process.env.NEXT_PUBLIC_WEBSITE_NAME || 'Animeshon Encyclopedia';
+
+const Search = ({ queryTime, results, hasMore, page }) => {
+    const { search } = useContext(SearchContext);
+    const router = useRouter();
+
+    const url = uri.AbsoluteURI(router.pathname);
+
+    const seo = {
+        siteName: WEBSITE_NAME,
+        title: `${WEBSITE_NAME} | Search results for ${search.search}`,
+        description: `The Japanese Multimedia Culture search engine`,
+        canonical: undefined,
+        url: url,
+        image: '../../public/brand/animeshon-brand-horizontal-small.svg',
+    };
+
     return (
         <>
+            <Head>
+                <title>{seo.title}</title>
+                {seo.canonical ? (<link rel="canonical" href={seo.canonical} />) : undefined}
+
+                {/* SEO */}
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                {seo.description ? (<meta name="description" content={seo.description} />) : undefined}
+
+                {/* Social Media & SEO */}
+                <meta property="og:site_name" content={seo.siteName}></meta>
+                <meta property="og:title" content={seo.title} />
+                {seo.description ? (<meta property="og:description" content={seo.description} />) : undefined}
+                {seo.image ? (<meta property="og:image" content={seo.image} />) : undefined}
+                {seo.url ? <meta property="og:url" content={seo.url} /> : undefined}
+
+                {/* Twitter */}
+                <meta name="twitter:card" content={seo.image ? 'summary_large_image' : 'summary'} />
+                <meta name="twitter:site" content="@AnimeshonSNS" />
+                {seo.twitter ? (<meta name="twitter:creator" content={seo.twitter} />) : undefined}
+                {seo.image ? (<meta name="twitter:image:alt" content={seo.title} />) : undefined}
+
+                {/* Facebook */}
+                {/* <meta property="fb:app_id" content="your_app_id" /> */}
+            </Head>
+
             <Header isSearchAvailable />
             <div className="header_padder" />
-            <ResultMetrics queryTime={queryTime}/>
+            <ResultMetrics queryTime={queryTime} />
             <div className="results-container">
                 <div className="left-column">
-                   <ResultDisplayer results={results} searchTerm={searchTerm} hasMore={hasMore} page={page}/>
+                    <ResultDisplayer results={results} searchTerm={search.search} hasMore={hasMore} page={page} />
                 </div>
                 <div className="right-column">
                     {/* TODO Universes */}
@@ -49,7 +94,7 @@ Search.getInitialProps = async ctx => {
     const { results, hasMore } = await SearchQuery(ctx, searchTerm, page, []);
     const queryTime = (Date.now() - startTime) / 1000.0; // in ms 
 
-    return { queryTime, results, hasMore, searchTerm, page };
+    return { queryTime, results, hasMore, page };
 };
 
 const SearchQuery = async (ctx, searchTerm, pages, filter) => {
