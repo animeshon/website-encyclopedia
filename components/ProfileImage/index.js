12b9364448129baf35089e34955de2d6993cb949
Luscha <matteo.roggia@gmail.com>
2020-12-02 23:15:42 +0100

    renamed class to better reflect the role of the div product-cover => container-side

--- components/ProfileImage/index.js
+++ components/ProfileImage/index.js
@@ -4,8 +4,8 @@ import SafeImage from '@/components/SafeImage';
 
 const ProfileImage = ({ image, altText }) => {
     return (
-        <div className="product-cover">
-            <figure className="product-cover__image">
+        <div className="container-side">
+            <figure className="container-side__image">
                 <SafeImage image={image} altText={altText} force={true} displayButton={true}/>
             </figure>
         </div>
