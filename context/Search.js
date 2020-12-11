0a4c57a899541bbff8b76b22f530db91fb0a1594
Luscha <matteo.roggia@gmail.com>
2020-12-11 18:38:53 +0100

    change of the search term do not caouse the reset of the filter

--- context/Search.js
+++ context/Search.js
@@ -5,7 +5,7 @@ export const SearchContext = React.createContext({});
 export const searchReducer = (state, action) => {
     switch (action.type) {
         case 'performNewSearch':
-            return { ...state, search: action.payload, filter: undefined };
+            return { ...state, search: action.payload };
         case 'applyFilter':
             return { ...state, filter: action.payload };
         default:
