886e2ed1f4b18145d78bdb37f2be789df7c15706
Luscha <matteo.roggia@gmail.com>
2020-12-13 00:23:09 +0100

    improved un restriction label

--- utilities/Restriction.js
+++ utilities/Restriction.js
@@ -3,7 +3,7 @@ export const Restrictions = (r) => {
     r.forEach(element => {
         switch (element.tag) {
             case "MINOR-R18":
-                res.push("Illegal");
+                res.push("Prohibited in multiple regions according to U.N. guidelines");
         }
     });
 
