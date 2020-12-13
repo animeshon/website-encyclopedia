de854794f1ec48f591d879f1c231cf82a54f2891
Luscha <matteo.roggia@gmail.com>
2020-12-13 21:40:53 +0100

    restriction is now undefined safe

--- utilities/Restriction.js
+++ utilities/Restriction.js
@@ -1,6 +1,6 @@
 export const Restrictions = (r) => {
     let res = [];
-    r.forEach(element => {
+    (r || []).forEach(element => {
         switch (element.tag) {
             case "MINOR-R18":
                 res.push("Prohibited in multiple regions according to U.N. guidelines");
