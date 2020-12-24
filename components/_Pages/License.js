0868cc680c75032313b9323bd6f884fede4e3683
Christian Roggia <christian.roggia@gmail.com>
2020-12-24 03:48:43 +0100

    Improve transparency and add direct links

--- components/_Pages/License.js
+++ components/_Pages/License.js
@@ -16,25 +16,24 @@ const License = () => {
                     <p>
                         This page lists all licenses and external work used throught our encyclopedia.<br /><br />
                     Please note that information available within our database is the result of an extensive work of data pre-processing (both automated and manual) and cross-referencing of existing records available among multiple different and publicly available sources.<br />
-                    All data owned by Animeshon is publicly available under <Link href='https://opendatacommons.org/licenses/dbcl/1-0/'>Open Database License</Link> and <Link href='https://opendatacommons.org/licenses/dbcl/1-0/'>Database Contents License</Link>.<br /><br />
-                    Finally, please note that all data available on this website is not coming from single sources (i.e. copycat database) but is a comprehensive database of all information shared by the community on a global scale and from different reliable data providers.
+                    All data owned by Animeshon is publicly available under <Link href='https://opendatacommons.org/licenses/dbcl/1-0/'>Open Database License</Link> and <Link href='https://opendatacommons.org/licenses/dbcl/1-0/'>Database Contents License</Link>.
                 </p>
                 </section>
                 <section>
                     <h3>Anime</h3>
-                    <p>Our database might contain external content and information extracted from various different sources which might include <strong>Wikipedia</strong>, <strong>AniDB</strong>, <strong>MyAnimeList</strong>, and <strong>Anime News Network</strong>.</p>
+                    <p>Our database might contain external content and information extracted from various different sources which might include <strong>Wikipedia</strong>, <strong><Link href="https://anidb.net/">AniDB</Link></strong>, <strong><Link href="https://myanimelist.net/">MyAnimeList</Link></strong>, and <strong><Link href="https://www.animenewsnetwork.com/">Anime News Network</Link></strong>.</p>
                 </section>
                 <section>
                     <h3>Manga, Light Novels, and Doujinshi</h3>
-                    <p>Our database might contain external content and information extracted from various different sources which might include <strong>Wikipedia</strong>, <strong>Baka-Updates Manga</strong>, <strong>MyAnimeList</strong>, <strong>The Light Novel Database</strong>, <strong>The Doujinshi &amp; Manga Lexicon</strong>, and <strong>Anime News Network</strong>.</p>
+                    <p>Our database might contain external content and information extracted from various different sources which might include <strong>Wikipedia</strong>, <strong><Link href="https://www.mangaupdates.com/index.html">Baka-Updates Manga</Link></strong>, <strong><Link href="https://myanimelist.net/">MyAnimeList</Link></strong>, <strong><Link href="https://lndb.info/">The Light Novel Database</Link></strong>, <strong><Link href="https://www.doujinshi.org/">The Doujinshi &amp; Manga Lexicon</Link></strong>, and <strong><Link href="https://www.animenewsnetwork.com/">Anime News Network</Link></strong>.</p>
                 </section>
                 <section>
                     <h3>Visual Novels</h3>
-                    <p>Our database might contain external content and information extracted from various different sources which might include <strong>Wikipedia</strong>, <strong>The Visual Novel Database</strong>, which is made available here under the Open Database License (ODbL).</p>
+                    <p>Our database might contain external content and information extracted from various different sources which might include <strong>Wikipedia</strong>, <strong><Link href="https://vndb.org/">The Visual Novel Database</Link></strong>, which is made available here under the Open Database License (ODbL).</p>
                 </section>
                 <section>
                     <h3>Tracks, Songs, and OSTs</h3>
-                    <p>Our database might contain external content and information extracted from various different sources which might include <strong>Wikipedia</strong>, <strong>AniDB</strong>, and <strong>VGMdb</strong>.</p>
+                    <p>Our database might contain external content and information extracted from various different sources which might include <strong>Wikipedia</strong>, <strong><Link href="https://anidb.net/">AniDB</Link></strong>, and <strong><Link href="https://vgmdb.net/">VGMdb</Link></strong>.</p>
                 </section>
                 <br />
                 <section>
