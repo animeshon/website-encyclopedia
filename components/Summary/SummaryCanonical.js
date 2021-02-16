539519991c34822a694da9c93694be87fdf1ded5
Luscha <matteo.roggia@gmail.com>
2021-02-16 13:47:56 +0100

    hide canonical section if no canonical are present

--- components/Summary/SummaryCanonical.js
+++ components/Summary/SummaryCanonical.js
@@ -11,20 +11,23 @@ const SummaryCanonical = ({ canonicals, path = 'franchise' }) => {
     const href = uri.Rewrite(container.type, container.title, container.id, path);
 
     return (
-        <section className="landing-section-box">
-            <header>
-                <h3>Series</h3>
-                <span />
-                <Link href={href}>
-                    <a className="view-all-link">View all</a>
-                </Link>
-            </header>
-            <ul className="adaptations-list">
-                {canonicals.map(canon => {
-                    return (<CanonicalPreview key={canon.id} canon={canon} />);
-                })}
-            </ul>
-        </section>
+        <>
+            {canonicals && canonicals.length ?
+                <section className="landing-section-box">
+                    <header>
+                        <h3>Series</h3>
+                        <span />
+                        <Link href={href}>
+                            <a className="view-all-link">View all</a>
+                        </Link>
+                    </header>
+                    <ul className="adaptations-list">
+                        {canonicals.map(canon => {
+                            return (<CanonicalPreview key={canon.id} canon={canon} />);
+                        })}
+                    </ul>
+                </section> : undefined}
+        </>
     );
 };
 
