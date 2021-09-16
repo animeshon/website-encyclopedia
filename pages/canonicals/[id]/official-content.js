a2cf027d82dc81be3ad8e11b8a56a969e9146b74
Luscha <matteo.roggia@gmail.com>
2021-09-16 16:21:03 +0200

    fix: build

--- pages/canonicals/[id]/official-content.js
+++ pages/canonicals/[id]/official-content.js
@@ -1,40 +1,40 @@
 import withContainer, { withContainerProps } from '@/components/Container';
 import CanonizableContent from '@/components/Canonical/CanonizableContent';
 
-import * as locale from '@/utilities/Localization';
-import { ByContent } from '@/utilities/Premiere';
-import * as stat from '@/utilities/ContentStatus';
-import { Type } from '@/utilities/MediaType';
+// import * as locale from '@/utilities/Localization';
+// import { ByContent } from '@/utilities/Premiere';
+// import * as stat from '@/utilities/ContentStatus';
+// import { Type } from '@/utilities/MediaType';
 
-import getContent from '@/queries/canonical/Content';
-import { ExecuteQuery, PrepareQuery } from '@/utilities/Query';
+// import getContent from '@/queries/canonical/Content';
+// import { ExecuteQuery, PrepareQuery } from '@/utilities/Query';
 
 
 export const getProps = async (ctx, client) => {
-    const { id } = ctx.query;
-    const data = await ExecuteQuery(client, PrepareQuery({ id: id }, getContent()));
+    // const { id } = ctx.query;
+    // const data = await ExecuteQuery(client, PrepareQuery({ id: id }, getContent()));
 
-    const contents = (data.contents || []).map(i => {
-        const { id, __typename, status, runnings, images, names, descriptions, ageRatings, releaseDate } = i;
-        if (names.length === 0 || __typename == "Doujinshi") {
-            return;
-        }
-        return {
-            id: id,
-            type: __typename,
-            name: locale.EnglishAny(names),
-            description: locale.English(descriptions),
-            // image: image.ProfileAny(images, ageRatings),
-            media: Type(__typename),
-            //type: Subtype(__typename, type),
-            releaseDate: ByContent(__typename, releaseDate, runnings),
-            status: stat.Status(status),
-        };
-    }).filter(c => { return c != undefined });
+    // const contents = (data.contents || []).map(i => {
+    //     const { id, __typename, status, runnings, images, names, descriptions, ageRatings, releaseDate } = i;
+    //     if (names.length === 0 || __typename == "Doujinshi") {
+    //         return;
+    //     }
+    //     return {
+    //         id: id,
+    //         type: __typename,
+    //         name: locale.EnglishAny(names),
+    //         description: locale.English(descriptions),
+    //         // image: image.ProfileAny(images, ageRatings),
+    //         media: Type(__typename),
+    //         //type: Subtype(__typename, type),
+    //         releaseDate: ByContent(__typename, releaseDate, runnings),
+    //         status: stat.Status(status),
+    //     };
+    // }).filter(c => { return c != undefined });
 
     return {
-        contents: contents,
-        title: "Official Content"
+        // contents: contents,
+        // title: "Official Content"
     };
 };
 
--- pages/universes/[id]/index.js
+++ pages/universes/[id]/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import withContainer, { withContainerProps } from '@/components/Container';
-import getSummary from '@/queries/universe/Summary';
+// import getSummary from '@/queries/universe/Summary';
 
 import SummaryText from '@/components/Summary/SummaryText';
 import SummaryCharacter from '@/components/Summary/SummaryCharacter';
@@ -35,57 +35,57 @@ const Canonical = ({
 };
 
 export const getProps = async (ctx, client) => {
-    const { id } = ctx.query;
-    const queries = [
-        PrepareKeyQuery("info", { id: id }, getSummary()),
-    ];
-    const { info } = await ExecuteQueryBatch(client, queries);
+    // const { id } = ctx.query;
+    // const queries = [
+    //     PrepareKeyQuery("info", { id: id }, getSummary()),
+    // ];
+    // const { info } = await ExecuteQueryBatch(client, queries);
 
-    const characters = [];
-    info.contents.forEach(c => {
-        c.starring.forEach(s => {
-            const { id, images, names } = s.character;
-            const hit = characters.filter(c => {return c.id == id});
-            if (hit.length != 0) {
-                hit[0].occurencies++;
-            } else {
-                characters.push({
-                    id: id,
-                    name: locale.LatinAny(names),
-                    image: image.ProfileAny(images),
-                    occurencies: 1,
-                })
-            }
-        })
-    })
+    // const characters = [];
+    // info.contents.forEach(c => {
+    //     c.starring.forEach(s => {
+    //         const { id, images, names } = s.character;
+    //         const hit = characters.filter(c => {return c.id == id});
+    //         if (hit.length != 0) {
+    //             hit[0].occurencies++;
+    //         } else {
+    //             characters.push({
+    //                 id: id,
+    //                 name: locale.LatinAny(names),
+    //                 image: image.ProfileAny(images),
+    //                 occurencies: 1,
+    //             })
+    //         }
+    //     })
+    // })
 
-    const canonicals = (info.canonicals || []).map(canonical => {
-        const { id, __typename, images, names, ageRatings } = canonical;
-        return {
-            id: id,
-            type: __typename,
-            name: locale.EnglishAny(names),
-            // image: image.ProfileAny(images, ageRatings),
-        }
-    });
+    // const canonicals = (info.canonicals || []).map(canonical => {
+    //     const { id, __typename, images, names, ageRatings } = canonical;
+    //     return {
+    //         id: id,
+    //         type: __typename,
+    //         name: locale.EnglishAny(names),
+    //         // image: image.ProfileAny(images, ageRatings),
+    //     }
+    // });
 
     return {
-        description: locale.English(info.descriptions),
-        characters: characters.sort((a, b) => {
-            return a.occurencies < b.occurencies;
-        }).slice(0, 5),
-        canonicals: canonicals,
-        details: [
-            [
-                { key: 'English', value: locale.English(info.names) },
-                { key: 'Japanese', value: locale.Japanese(info.names) },
-                { key: 'Romaji', value: locale.Romaji(info.names) },
-            ],
-            [
-                { key: 'Contents', value: info.contentsAggregate?.count || 0 },
-                { key: 'Series', value: info.canonicalsAggregate?.count || 0 },
-            ]
-        ]
+        // description: locale.English(info.descriptions),
+        // characters: characters.sort((a, b) => {
+        //     return a.occurencies < b.occurencies;
+        // }).slice(0, 5),
+        // canonicals: canonicals,
+        // details: [
+        //     [
+        //         { key: 'English', value: locale.English(info.names) },
+        //         { key: 'Japanese', value: locale.Japanese(info.names) },
+        //         { key: 'Romaji', value: locale.Romaji(info.names) },
+        //     ],
+        //     [
+        //         { key: 'Contents', value: info.contentsAggregate?.count || 0 },
+        //         { key: 'Series', value: info.canonicalsAggregate?.count || 0 },
+        //     ]
+        // ]
     };
 };
 
