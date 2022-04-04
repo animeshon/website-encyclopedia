ad3fbcea909e7c4c47683a666c70578da79db039
Christian Roggia <christian.roggia@gmail.com>
2022-04-04 17:47:15 +0200

    fix: remove pages case sensitive duplicates

--- pages/lightnovels/[id]/characters.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import withContainer, { withContainerProps } from '@/components/Container';
-import CharacterPage, { getProps } from '@/components/Character/CharacterPage';
-
-export default withContainer(CharacterPage);
-export const getServerSideProps = withContainerProps(getProps);
--- pages/visualnovels/[id]/releases.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import withContainer, { withContainerProps } from '@/components/Container';
-import ReleasePage, { getProps } from '@/components/Release/ReleasePage';
-
-export default withContainer(ReleasePage);
-export const getServerSideProps = withContainerProps(getProps);
